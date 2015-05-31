8a77f886a83c0809c40b9d78019658b2a6cd9fb6
RoxasShadow <webmaster@giovannicapuano.net>
2015-05-31 13:20:51 +0200

    test_backlog: fix

--- tests/test_backlog.rs
+++ tests/test_backlog.rs
@@ -5,12 +5,7 @@ use cleverbot::Cleverbot;
 #[test]
 pub fn backlog() {
   let mut cleverbot = Cleverbot::new();
-  let mut reply;
-
-  while {
-    reply = cleverbot.think("How're you today?".to_string());
-    reply.answer.is_empty()
-  } {}
+  cleverbot.think("How're you today?".to_string());
 
   assert!(cleverbot.backlog.len() == 1);
 }
